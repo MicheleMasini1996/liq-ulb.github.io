4d1e5ee3941104f60a45bc35dc5a9fbe3d2ca6a6
Stefano Pironio <stefano.pironio@ulb.be>
2022-03-08 10:42:09 +0100

    small mod to arxiv2bib (strings are immutable)

--- utils/arxiv2bib.jl
+++ utils/arxiv2bib.jl
@@ -1,39 +1,9 @@
 # The python version used by PyCall must have the following packages installed
 # arxiv
 
-# using Dates, AnyAscii, PyCall
-# arxiv = pyimport("arxiv")
-# urllib = pyimport("urllib")
-
-"""
-Obtain bibtex string from doi through crossref.org
-For journals of the APS family the "pages" field is missing. A simple fix is provided below (see add_pages_bibstr!) using the fact that the last part of the doi correspond to the page value for APS journals.
-"""
-function doi2bib(doi)
-    doi_req = urllib.request.Request("http://dx.doi.org/"*doi)
-    doi_req.add_header("Accept", "application/x-bibtex")
-    f=urllib.request.urlopen(doi_req)
-    f.read()
-end
-
-function add_data_bibstr!(bibstr,data)
-    bibstr=bibstr[1:end-2]*",\n"*data*"\n}"
-end
-
-function add_arxiv_bibstr!(bibstr,paper)
-    arxivdata = "\teprint = {$(paper.get_short_id())},\n\tarchivePrefix = {arXiv},\n\tprimaryClass ={$(paper.categories[1])}"
-    add_data_bibstr!(bibstr,arxivdata)
-end
-
-"Add the pages field if it is missing using the last part of the doi"
-function add_pages_bibstr!(bibstr,doi)
-    if occursin("pages",bibstr)
-        return bibstr
-    else
-        pages = "\tpages = {$(split(doi,".")[end])}"
-        add_data_bibstr!(bibstr,pages)
-    end
-end
+using Dates, AnyAscii, PyCall
+arxiv = pyimport("arxiv")
+urllib = pyimport("urllib")
 
 function arxiv2bib(paper)
     author_list = join([author.name for author in paper.authors]," and ")
@@ -49,21 +19,58 @@ function arxiv2bib(paper)
             primaryClass ={$(paper.categories[1])}\n}"
 end
 
-function writebib(filename,search)
-    file=open(filename,"w")
-    for paper in search.results()
-        if isnothing(paper.doi)
-            write(file,arxiv2bib(paper),"\n\n")
-        else
-            bibstr=doi2bib(paper.doi)
-            bibstr=add_pages_bibstr!(bibstr,paper.doi)
-            bibstr=add_arxiv_bibstr!(bibstr,paper)
-            write(file,bibstr,"\n\n")
-        end
-    end
-    close(file)
+"""
+Obtain bibtex string from doi through crossref.org
+For journals of the APS family the "pages" field is missing. A simple fix is provided below (see add_pages_bibstr!) using the fact that the last part of the doi correspond to the page value for APS journals.
+"""
+function doi2bib(doi)
+    doi_req = urllib.request.Request("http://dx.doi.org/"*doi)
+    doi_req.add_header("Accept", "application/x-bibtex")
+    f=urllib.request.urlopen(doi_req)
+    f.read()
 end
 
-filename="pub.bib"
-search = arxiv.Search(query="au:Pironio",sort_by = arxiv.SortCriterion.SubmittedDate)
-writebib(filename,search)
+function add_data_bibstr(bibstr,data)
+    bibstr=bibstr[1:end-2]*",\n"*data*"\n}"
+end
+
+function add_arxiv_bibstr(bibstr,paper)
+    arxivdata = "\teprint = {$(paper.get_short_id())},\n\tarchivePrefix = {arXiv},\n\tprimaryClass ={$(paper.categories[1])}"
+    add_data_bibstr(bibstr,arxivdata)
+end
+
+"Add the pages field if it is missing using the last part of the doi"
+function add_pages_bibstr(bibstr,doi)
+    if occursin("pages",bibstr)
+        return bibstr
+    else
+        pages = "\tpages = {$(split(doi,".")[end])}"
+        add_data_bibstr(bibstr,pages)
+    end
+end
+
+
+function search2bib(search) 
+    bibstr="";
+    for paper in search.results()
+        if isnothing(paper.doi)
+           bibstr=string(bibstr,arxiv2bib(paper),"\n\n")
+        else
+            bibstr=string(bibstr,doi2bib(paper.doi))
+            bibstr=add_pages_bibstr(bibstr,paper.doi)
+            bibstr=add_arxiv_bibstr(bibstr,paper)
+            bibstr=string(bibstr,"\n\n")
+        end
+    end
+    return bibstr
+end
+
+search = arxiv.Search(query="au:Navascues AND au:vertesi AND bounding",sort_by = arxiv.SortCriterion.SubmittedDate)
+bibstr=search2bib(search)
+print(bibstr)
+
+# filename="pub_serge.bib"
+# file=open(filename,"w")
+# write(file,bibstr,"\n\n")
+# close(file)
+
